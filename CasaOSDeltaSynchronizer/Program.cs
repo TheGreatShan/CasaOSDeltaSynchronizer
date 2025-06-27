5a51a7f0625dc306481116d24c9f95710a5b45bf
Shansai Muraleetharan <shansai@muraleetharan.ch>
2025-06-27 18:49:27 +0200

    cancellation token has been added

--- CasaOSDeltaSynchronizer/Program.cs
+++ CasaOSDeltaSynchronizer/Program.cs
@@ -8,9 +8,19 @@ public class Program
         var appSettings = AppSettingsReader.Read("./appsettings.json");
         var watcher = new Watcher.Watcher(appSettings.ClientLocation);
 
-        while (true)
+        var cancellationTokenSource = new CancellationTokenSource();
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            e.Cancel = true;
+            cancellationTokenSource.Cancel();
+        };
+        
+        while (!cancellationTokenSource.Token.IsCancellationRequested)
         {
             watcher.ChangedFilePaths.ForEach(Console.WriteLine);
+            Thread.Sleep(1000); 
         }
+
+        Console.WriteLine("File copying exited");
     }
 }
