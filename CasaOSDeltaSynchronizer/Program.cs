34634a3889dde5da47de4ba2cb76459852a85876
Shansai Muraleetharan <shansai@muraleetharan.ch>
2025-06-27 18:42:54 +0200

    the changed file paths are looped through till stopped

--- CasaOSDeltaSynchronizer/Program.cs
+++ CasaOSDeltaSynchronizer/Program.cs
@@ -1,8 +1,16 @@
-﻿namespace CasaOSDeltaSynchronizer; 
+﻿using CasaOSDeltaSynchronizer.Services;
+
+namespace CasaOSDeltaSynchronizer; 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
+        var appSettings = AppSettingsReader.Read("./appsettings.json");
+        var watcher = new Watcher.Watcher(appSettings.ClientLocation);
+
+        while (true)
+        {
+            watcher.ChangedFilePaths.ForEach(Console.WriteLine);
+        }
     }
 }
